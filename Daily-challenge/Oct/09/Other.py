c793ddc07bbbb23c3a7c426c66caf99345b7e3d4
James ~ <zqxhjjh@outlook.com>
2020-10-09 22:06:44 +1300

    update solution

---
+++ Daily-challenge/Oct/09/Other.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    def serialize(self, root):    
+        if not root: return ""
+        q = collections.deque([root])
+        res = []
+        while q:
+            node = q.popleft()
+            if node:
+                q.append(node.left)
+                q.append(node.right)
+            res.append(str(node.val) if node else '#')
+        return ','.join(res)
+    def deserialize(self, data: str) -> TreeNode:
+        """Decodes your encoded data to tree.
+        """
+        if not data: return None
+        nodes = data.split(',')
+        root = TreeNode(int(nodes[0]))
+        q = collections.deque([root])
+        index = 1
+        while q:
+            node = q.popleft()
+            if nodes[index] is not '#':
+                node.left = TreeNode(int(nodes[index]))
+                q.append(node.left)
+            index += 1
+        
+            if nodes[index] is not '#':
+                node.right = TreeNode(int(nodes[index]))
+                q.append(node.right)
+            index += 1
+        return root
+        
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
