e97b16e84cb97073d52dbeb6f37d48fe4e979c02
James ~ <zqxhjjh@outlook.com>
2020-11-29 18:51:55 +1300

    29/11/2020

--- Daily-challenge/Nov/28/sol.py
+++ Daily-challenge/Nov/28/sol.py
@@ -1,19 +1,32 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        sumn = sum(nums) 
-        if sumn % 2 or len(nums) < 2 :
-            return False
+    def maxSlidingWindow(self, nums: List[int], w: int) -> List[int]:
+        max_left = [0 for n in range(len(nums))]
+        max_right = [0 for n in range(len(nums))]
+        max_left[0] = nums[0]
+        max_right[-1] = nums[-1]
         
-        subSum = sumn // 2
-        nums.sort(reverse = True)
-        
-        for i in range(1 ,len(nums)) :
-            tmp = nums[0]
-            for j in range(i ,len(nums)) :
-                tmp += nums[j]
-                if tmp > subSum :
-                    tmp -= nums[j]
-                elif tmp == subSum :
-                    return True
-        
-        return nums[0] == subSum
+        for i in range(1, len(nums)):
+            if i % w == 0:
+                
+                max_left[i] = nums[i]
+            else:
+                max_left[i] = max(nums[i], max_left[i-1])
+                
+            
+            j = len(nums) - i - 1
+            
+            if j % w == 0:
+                max_right[j] = nums[j]
+            else:
+                max_right[j] = max(nums[j], max_right[j+1])
+        # print(max_right, max_left)
+        lst = []
+        x = 0
+        # j = 0
+        while x + w <= len(nums):
+            # j += 1
+            # print(j)
+            lst += [max(max_right[x], max_left[x+w-1])]
+            x += 1
+            
+        return lst
