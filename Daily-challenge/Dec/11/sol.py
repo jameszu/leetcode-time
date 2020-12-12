343c4a87b6b3fabbed7a512dfba52c2050bf33e2
James ~ <zqxhjjh@outlook.com>
2020-12-12 21:29:04 +1300

    12/12/2020

--- Daily-challenge/Dec/11/sol.py
+++ Daily-challenge/Dec/11/sol.py
@@ -1,11 +1,23 @@
 class Solution:
-    def validSquare(self, p1, p2, p3, p4):
-        if p1==p2==p3==p4:return False
-        def dist(x,y):
-            return (x[0]-y[0])**2+(x[1]-y[1])**2
-        ls=[dist(p1,p2),dist(p1,p3),dist(p1,p4),dist(p2,p3),dist(p2,p4),dist(p3,p4)]
-        ls.sort()
-        if ls[0]==ls[1]==ls[2]==ls[3]:
-            if ls[4]==ls[5]:
-                return True
-        return False
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+        idx = 0
+        
+        count = 0
+        
+        while idx < len(nums)-1:
+            # print(idx)
+            if nums[idx] == nums[idx+1]:
+                count += 1
+            if nums[idx] != nums[idx+1]:
+                count = 0
+
+                
+            if count >= 2:
+                del nums[idx]
+            else:
+                idx += 1
+        return len(nums)
+                
+            
