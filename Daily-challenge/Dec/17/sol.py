9ecdb4351ff7e826b082e981f88ff0df759b3dc2
James ~ <zqxhjjh@outlook.com>
2020-12-18 22:44:54 +1300

    18/12/2020

--- Daily-challenge/Dec/17/sol.py
+++ Daily-challenge/Dec/17/sol.py
@@ -1,4 +1,18 @@
 class Solution:
-    def mirrorReflection(self, p: int, q: int) -> int:
-        while p % 2 == 0 and q % 2 == 0: p, q = p / 2, q / 2
-        return int(1 - p % 2 + q % 2)
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        count = 0
+        num_dict = {}
+        for i in C:
+            for j in D:
+                s = i + j
+                if s in num_dict:
+                    num_dict[s] += 1
+                else:
+                    num_dict[s] = 1
+
+        for i in range(len(A)):
+            for j in range(len(B)):
+                target = 0 - (A[i]+B[j])
+                if target in num_dict:
+                    count += num_dict[target]
+        return count
