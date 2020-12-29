e4b650f5a3727b50707a91ad52636ea602b501b4
James ~ <zqxhjjh@outlook.com>
2020-12-29 20:49:32 +1300

    back to business

--- Daily-challenge/Dec/28/sol.py
+++ Daily-challenge/Dec/28/sol.py
@@ -1,32 +1,11 @@
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], w: int) -> List[int]:
-        max_left = [0 for n in range(len(nums))]
-        max_right = [0 for n in range(len(nums))]
-        max_left[0] = nums[0]
-        max_right[-1] = nums[-1]
-        
-        for i in range(1, len(nums)):
-            if i % w == 0:
+    def reachNumber(self, target: int) -> int:
+        bound = ceil(sqrt(2*abs(target)+0.25) - 0.5)
+        if target % 2 == 0:
+            if bound % 4 == 1: bound += 2
+            if bound % 4 == 2: bound += 1
+        else:
+            if bound % 4 == 3: bound += 2
+            if bound % 4 == 0: bound += 1
                 
-                max_left[i] = nums[i]
-            else:
-                max_left[i] = max(nums[i], max_left[i-1])
-                
-            
-            j = len(nums) - i - 1
-            
-            if j % w == 0:
-                max_right[j] = nums[j]
-            else:
-                max_right[j] = max(nums[j], max_right[j+1])
-        # print(max_right, max_left)
-        lst = []
-        x = 0
-        # j = 0
-        while x + w <= len(nums):
-            # j += 1
-            # print(j)
-            lst += [max(max_right[x], max_left[x+w-1])]
-            x += 1
-            
-        return lst
+        return bound
